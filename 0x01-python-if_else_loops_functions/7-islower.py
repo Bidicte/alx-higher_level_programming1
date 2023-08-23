a668c69fe252968ea12e94fd8e053aee4696a314
Bidicte <malexandra13@yahoo.com>
2023-08-23 11:57:59 +0000

    7. islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
     for letter in c:
-        if not ('a' <= letter <= 'z'):
+        if not ('a' <= letter <= 'z') and not c:
             return False
         return True
