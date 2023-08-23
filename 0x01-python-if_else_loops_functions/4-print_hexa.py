7f92257c3da235cfed25dbb1f3f984552fc53884
Bidicte <malexandra13@yahoo.com>
2023-08-23 02:04:55 +0000

    4. Hexadecimal printing

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-for i in range(0,99):
-    if i > 9:
-        j = hex(i)
-        print(f"{i} = {j}")
-    else:
-        print(f"{i} = 0x{i}")
+for i in range(0, 99):
+    print("{} = {}".format(i, hex(i)))
